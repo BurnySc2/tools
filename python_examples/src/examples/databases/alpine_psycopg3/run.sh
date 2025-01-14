f64019dec246214e5f840a1d3a1a264a10337a33
burnysc2 <gamingburny@gmail.com>
2025-01-14 18:14:12 +0100

    Add slim build

--- python_examples/src/examples/databases/alpine_psycopg3/run.sh
+++ python_examples/src/examples/databases/alpine_psycopg3/run.sh
@@ -1,2 +1,8 @@
 docker build -t temp .
-docker run --rm -it --env-file "../../../../.env" temp
+
+# Compress image with slim
+slim build --target temp --tag temp_slim --http-probe=false --env-file "../../../../.env" --exec "python /root/main.py"
+# Ideally the following but it doesnt work, missing imports
+# slim build --target temp --tag temp_slim --http-probe=false --env STAGE=BUILD --exec "python /root/main.py"
+
+docker run --rm -it --env-file "../../../../.env" temp_slim
